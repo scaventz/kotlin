11b33d194f1e64b079a8ef3c203bbfb1aaa67a8d
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-12-03 16:52:58 +0300

    [CMI] Fix clearing code meta infos from original text

--- compiler/test-infrastructure-utils/tests/org/jetbrains/kotlin/codeMetaInfo/CodeMetaInfoRenderer.kt
+++ compiler/test-infrastructure-utils/tests/org/jetbrains/kotlin/codeMetaInfo/CodeMetaInfoRenderer.kt
@@ -105,4 +105,4 @@ fun clearFileFromDiagnosticMarkup(file: File) {
     file.writeText(cleanText)
 }
 
-fun clearTextFromDiagnosticMarkup(text: String): String = CheckerTestUtil.rangeStartOrEndPattern.matcher(text).replaceAll("")
+fun clearTextFromDiagnosticMarkup(text: String): String = text.replace(CodeMetaInfoParser.openingOrClosingRegex, "")
