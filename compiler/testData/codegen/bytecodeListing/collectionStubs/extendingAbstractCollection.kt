0aafd828a0294870a04abf4e07258b5e760a6d09
Xin Wang <scaventz@outlook.com>
2023-04-13 11:02:00 +0800

    box test

--- compiler/testData/codegen/bytecodeListing/collectionStubs/extendingAbstractCollection.kt
+++ compiler/testData/codegen/bytecodeListing/collectionStubs/extendingAbstractCollection.kt
@@ -1,4 +1,3 @@
-// IGNORE_BACKEND_K2: JVM_IR
 // FIR status: KT-57299 K2: VerifyError due to overriding final method `size` on a subclass of Collection and Set
 
 abstract class AC<X> : Collection<X>
